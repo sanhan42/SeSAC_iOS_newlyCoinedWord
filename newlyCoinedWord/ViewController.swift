15fc11abc75e2790d6a57544d054698d73e791f0
sanhan42 <sangmin613@gmail.com>
2022-07-12 19:43:03 +0900

    Add Search Button Action

--- newlyCoinedWord/ViewController.swift
+++ newlyCoinedWord/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet var buttonList: [UIButton]!
+    @IBOutlet weak var resultLable: UILabel!
     
     let newlyWord = ["억텐":"'억지 텐션'의 줄임말.", "찐텐":"'진짜 텐션'의 줄임말", "당모치":"'당연히 모든 치킨은 옳다' = 뭘 시켜도 맛있을 것이다.", "식집사":"반려 식물을 키우는 사람.","자컨":"자체 제작 컨텐츠. 소속사에서 직접 만드는 컨텐츠.","머선129":"무슨 일이고!","알잘딱깔센":"'알아서 잘 딱 깔끔하고 센스있게'를 줄인 말.","좋댓구알":"좋아요, 댓글, 구독, 알림설정을 요청할 때 사용."]
     
@@ -19,6 +20,19 @@ class ViewController: UIViewController {
         setPlaceholder(textField: searchTextField, placeholder: "검색어를 입력해주세요.", color: .gray)
         setButtonTitle()
         setButtonStyle()
+    }
+    
+    @IBAction func searchButtonClicked(_ sender: UIButton) {
+        if searchTextField.text == "" {
+            setButtonTitle()
+            resultLable.text = nil
+            return ;
+        }
+        if let result = newlyWord[searchTextField.text!]{
+            setButtonTitle()
+            searchTextField.text = nil
+            resultLable.text = result
+        }
     }
     
     func setPlaceholder(textField tf:UITextField, placeholder str:String, color:UIColor) {
