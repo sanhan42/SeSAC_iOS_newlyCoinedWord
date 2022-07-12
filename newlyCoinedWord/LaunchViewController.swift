1d2f6df429812914dea3de16574c04f421d7f425
sanhan42 <sangmin613@gmail.com>
2022-07-12 18:09:42 +0900

    edit LaunchScreen

---
+++ newlyCoinedWord/LaunchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LaunchViewController.swift
+//  newlyCoinedWord
+//
+//  Created by 한상민 on 2022/07/12.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sleep(3)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
